b0f321a901c7a8f5bd8b0b0618cac017617378ca
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-09-03 18:01:08 +0700

    update video streaming && video hls

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,14 @@ import { Link } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "vidstack/styles/defaults.css";
+import "vidstack/styles/community-skin/video.css";
+import {
+  MediaCommunitySkin,
+  MediaOutlet,
+  MediaPlayer,
+  MediaPoster,
+} from "@vidstack/react";
 
 const getGoogleAuthUrl = () => {
   const { VITE_GOOGLE_CLIENT_ID, VITE_GOOGLE_REDIRECT_URI } = import.meta.env;
@@ -39,6 +47,42 @@ export default function Home() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </span>
       </div>
+      <h2>Video streaming</h2>
+      <video controls width={500}>
+        <source
+          src="http://localhost:4040/static/video-stream/SSVGTX_WmWO2Lkfs38dfZ.mp4"
+          type="video/mp4"
+        />
+      </video>
+
+      <h2>HLS Streaming</h2>
+      <MediaPlayer
+        title="Sprite Fight"
+        src="http://localhost:4000/static/video-hls/KadPDrG-jYOY5wUSSuUZP/master.m3u8"
+        // poster='https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/thumbnail.webp?time=268&width=980'
+        // thumbnails='https://media-files.vidstack.io/sprite-fight/thumbnails.vtt'
+        aspectRatio={16 / 9}
+        crossorigin=""
+      >
+        <MediaOutlet>
+          <MediaPoster alt="Girl walks into sprite gnomes around her friend on a campfire in danger!" />
+          {/* <track
+            src='https://media-files.vidstack.io/sprite-fight/subs/english.vtt'
+            label='English'
+            srcLang='en-US'
+            kind='subtitles'
+            default
+          /> */}
+          {/* <track
+            src='https://media-files.vidstack.io/sprite-fight/chapters.vtt'
+            srcLang='en-US'
+            kind='chapters'
+            default
+          /> */}
+        </MediaOutlet>
+        <MediaCommunitySkin />
+      </MediaPlayer>
+
       <h1>Google OAuth 2.0</h1>
       <p className="read-the-docs">
         {isAuthenticated ? (
